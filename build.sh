3bce372e4c176034f820ee44f0bf074e7987070f
dexy <tglee1127@naver.com>
2026-08-25 12:31:19 +0900

    docs: move scoop guide to ROS2 README and remove build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-source /opt/ros/jazzy/setup.bash
-source /home/dexy/ws_cobot_pjt/ws_dsr/install/setup.bash
-cd "$WORKSPACE_DIR"
-colcon build --packages-select icecream_interfaces ice_cream_pj --symlink-install
